ab106b3373ff94b92a9952b36d6ab442c6db056a
Alp Tuna Şahin <118995494+alpgithubx@users.noreply.github.com>
2023-06-19 16:59:11 +0300

    main

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("Prime numbers between 1 and 100 are:", "\n");
+            for (int i = 1; i < 101; i++)
+            {
+                count = 0;
+                if (i > 1)
+                {
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            count = 1;
+                            break;
+                        }
+                    }
+                    if (count == 0)
+                    {
+                        Console.Write(i + "  ");
+                    }
+                }
+            }
+        }
+    }
+}
